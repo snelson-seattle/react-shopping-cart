419677d38847d19d4787984e4bd982724d2462d0
Scott Nelson <snelson@instructors.2u.com>
2023-01-10 13:59:11 -0800

    created scaffolding for Header, Footer, Cart, and ProductList components

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Cart from "./components/Cart";
+import ProductList from "./components/ProductList";
+
 function App() {
   return (
     <>
+      <Header />
       <h1 className="text-5xl">App goes here.</h1>
+      <Footer />
     </>
   );
 }
