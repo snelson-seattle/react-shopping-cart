329d9705519eabea501e505ba64609dcf06f706d
Scott Nelson <snelson@instructors.2u.com>
2023-01-14 11:36:28 -0800

    updated footer component to use useCart custom hook

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,10 +1,26 @@
+import useCart from "../hooks/useCart"
+
 type PropsType = {
     viewCart: boolean,
 }
 
 const Footer = ({viewCart}: PropsType) => {
+  const {totalItems, totalPrice} = useCart();
+  const year: number = new Date().getFullYear();
+
+
+  const content = viewCart ? <p>Shopping Cart &copy; {year}</p> : (
+    <>
+      <p>Total Items: {totalItems}</p>
+      <p>Total Price: {totalPrice}</p>
+      <p>Shopping Cart &copy; {year}</p>
+    </>
+  )
+
   return (
-    <footer></footer>
+    <footer className="flex flex-grow flex-nowrap justify-end">
+      {content}
+    </footer>
   )
 }
 
